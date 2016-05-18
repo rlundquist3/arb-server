8638e55327755004ba5c3f8849a5ba98df3cdc93
Riley Lundquist <rlundquist3@gmail.com>
2016-05-18 11:12:26 -0400

    set up flask api server

---
+++ arb_server.py
@@ -0,0 +1,21 @@
+from flask import request, url_for
+from flask.ext.api import FlaskAPI, status, exceptions
+
+app = FlaskAPI(__name__)
+
+about_text = '''Kalamazoo College's Lillian Anderson Arboretum comprises 140 acres of marsh, meadow, pine plantation and second-growth deciduous forest in Oshtemo Township, Kalamazoo County, Michigan.\n\n
+Several families worked the land between the early-19th and mid-20th centuries. Lillian Anderson, a K graduate, donated approximately 100 acres of her family's farm to the College in 1982. In 2000, thanks to the generous financial assistance of Dr. and Mrs. H. Lewis Batts, the College added 31 acres of land along the eastern boundary of the original Anderson property. The Arboretum was established in 1998 as a resource for the Kalamazoo College community, Kalamazoo-area residents, and visitors to the area.
+'''
+
+def string_rep(str):
+    return {
+        'url': request.host_url.rstrip('/') + url_for('about'),
+        'text': about_text
+    }
+
+@app.route('/about/', methods=['GET'])
+def about():
+    return string_rep(about_text)
+
+if __name__ == '__main__':
+    app.run()
