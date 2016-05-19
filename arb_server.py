fcbdcbb88b196c180852a3dd2ef6464d9c97a1c5
Riley Lundquist <rlundquist3@gmail.com>
2016-05-19 11:26:31 -0400

    trying mail

--- arb_server.py
+++ arb_server.py
@@ -1,7 +1,9 @@
 from flask import request, url_for
 from flask.ext.api import FlaskAPI, status, exceptions
+from flaskext.mail import Mail, Message
 
 app = FlaskAPI(__name__)
+mail = Mail(app)
 
 about_text = '''Kalamazoo College's Lillian Anderson Arboretum comprises 140 acres of marsh, meadow, pine plantation and second-growth deciduous forest in Oshtemo Township, Kalamazoo County, Michigan.\n\n
 Several families worked the land between the early-19th and mid-20th centuries. Lillian Anderson, a K graduate, donated approximately 100 acres of her family's farm to the College in 1982. In 2000, thanks to the generous financial assistance of Dr. and Mrs. H. Lewis Batts, the College added 31 acres of land along the eastern boundary of the original Anderson property. The Arboretum was established in 1998 as a resource for the Kalamazoo College community, Kalamazoo-area residents, and visitors to the area.
@@ -20,6 +22,20 @@ def home():
 @app.route('/about/', methods=['GET'])
 def about():
     return string_rep(about_text)
+
+@app.route('/mail/', methods=['POST'])
+def mail():
+    name = str(request.data.get('name', ''))
+    email = str(request.data.get('email', ''))
+    body = str(request.data.get('body', ''))
+
+    message = Message('Message from Arb App',
+                        sender=email,
+                        recipient='riley.lundquist12@kzoo.edu',
+                        body='Message from %s:\n\n%s' % (name, body))
+    mail.send(message)
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
