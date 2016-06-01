5427eb6e930e8a61d879e3edf256779d0b13e4e1
Riley Lundquist <rlundquist3@gmail.com>
2016-06-01 11:15:11 -0400

    testing file upload

--- arb_server.py
+++ arb_server.py
@@ -1,9 +1,17 @@
-from flask import request, url_for
+from flask import Flask, render_template, request, url_for
 from flask.ext.api import FlaskAPI, status, exceptions
 from flask.ext.mail import Mail, Message
+import shlex
+import subprocess
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = '/data/'
+ALLOWED_EXTENSIONS = set(['csv', 'json', 'kml'])
 
 app = FlaskAPI(__name__)
 mail = Mail(app)
+app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 about_text = '''Kalamazoo College's Lillian Anderson Arboretum comprises 140 acres of marsh, meadow, pine plantation and second-growth deciduous forest in Oshtemo Township, Kalamazoo County, Michigan.\n\n
 Several families worked the land between the early-19th and mid-20th centuries. Lillian Anderson, a K graduate, donated approximately 100 acres of her family's farm to the College in 1982. In 2000, thanks to the generous financial assistance of Dr. and Mrs. H. Lewis Batts, the College added 31 acres of land along the eastern boundary of the original Anderson property. The Arboretum was established in 1998 as a resource for the Kalamazoo College community, Kalamazoo-area residents, and visitors to the area.
@@ -15,9 +23,29 @@ def string_rep(str):
         'text': str
     }
 
-@app.route('/', methods=['GET'])
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+def db_import(filename):
+    args = ['mongoimport', '--db', 'arb',
+            '--type', filename.rsplit('.', 1)[1],
+            '--headerline', '--ignoreBlanks', 
+            '--file', filename]
+    proc = subprocess.Popen(args)
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return string_rep('connected to arb server')
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            db_import(filename)
+
+            return redirect(url_for('index'))
+    return render_template('main.jade')
 
 @app.route('/about/', methods=['GET'])
 def about():
